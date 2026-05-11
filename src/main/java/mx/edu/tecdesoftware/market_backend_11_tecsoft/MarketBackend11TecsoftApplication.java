9f47f428253af25af29e519507c98c0f87914738
EdwuardTSW <edwuard2213dk@gmail.com>
2026-05-11 08:25:55 -0600

    chore: initial Spring Boot project setup

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_11_tecsoft/MarketBackend11TecsoftApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_11_tecsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackend11TecsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackend11TecsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/tecdesoftware/market_backend_11_tecsoft/MarketBackend11TecsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_11_tecsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackend11TecsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
