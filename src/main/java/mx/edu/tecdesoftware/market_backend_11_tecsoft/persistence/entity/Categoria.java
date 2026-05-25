9667107b66c47eeb25a4bb7a0e1c9deef42d2ae9
EdwuardTSW <edwuard2213dk@gmail.com>
2026-05-25 08:58:44 -0600

    feat(persistence): add base entities

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend_11_tecsoft/persistence/entity/Categoria.java
@@ -0,0 +1,42 @@
+package mx.edu.tecdesoftware.market_backend_11_tecsoft.persistence.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias" )
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id_categoria")
+    private Integer idCategoria;
+
+    String descripcion;
+
+    private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
--- src/main/java/mx/edu/tecdesoftware/market_backend_11_tecsoft/persistence/entity/Producto.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_11_tecsoft/persistence/entity/Producto.java
@@ -1,10 +1,9 @@
-package mx.edu.tecdesoftware.market_backend_11_tecsoft.persistence.entity;
+package mx.edu.tecdesoftware.market_backend.persistence.entity;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.CollectionId;
 
 @Entity
-@Table(name = "productos" )
+@Table(name = "productos")
 public class Producto {
 
     @Id
@@ -12,20 +11,77 @@ public class Producto {
     @Column(name = "id_producto")
     private Integer idProducto;
 
+    @Column(name = "nombre")
     private String nombre;
 
     @Column(name = "id_categoria")
-    private Integer id_categoria;
+    private Integer idCategoria;
 
     @Column(name = "codigo_barras")
-    private Integer precioVenta;
+    private String codigoBarras;
 
     @Column(name = "precio_venta")
-    private Integer id_categoria;
+    private Double precioVenta;
 
     @Column(name = "cantidad_stock")
     private Integer cantidadStock;
 
+    @Column(name = "estado")
     private Boolean estado;
 
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
